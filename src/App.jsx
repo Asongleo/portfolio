b9a612abf7c4f822b073afea420fa5499f4aea75
Asongleo <120486708+Asongleo@users.noreply.github.com>
2023-06-06 12:05:59 -0500

    frontend completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import './index.css'
-import Header from './components/header/header'
-import Navbar from './components/navbar/navbar'
-import About from './components/about/about'
-import Experience from './components/experience/experience'
-import Services from './components/service/service'
-import Portfolio from './components/portfolio/portfolio'
-import Testimonial from './components/testimonial/testimonial'
-import Contact from './components/contact/contact'
-import Footer from './components/footer/footer'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/service/Service'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 function App () {
 return (
 <>
-    <Header />
-    
-{/* 
-<About/>
-<Experience/>
+    <Header /> 
+ <Navbar/>
+ <About/>
+  <Experience/>
 <Services/>
 <Portfolio/>
 <Testimonial/>
 <Contact/>
-<Footer/> */}
+<Footer/> 
 </>
 );
 }
